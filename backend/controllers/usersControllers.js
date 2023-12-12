071c15cf31b701aab3c0e5f34a374c7b3a1c867d
yeison <yeisonferneyhoyos@gmail.com>
2023-12-12 15:53:32 -0500

    ....

--- backend/controllers/usersControllers.js
+++ backend/controllers/usersControllers.js
@@ -1,11 +1,18 @@
 const User = require('../models/usersModel');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-// Controlador para crear un nuevo usuario
+const saltRounds = 10;
+const jwtSecret = process.env.JWT_SECRET || 'tu_jwt_secret'; // Guarda tu secreto en una variable de entorno
+
 exports.createUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
-    const user = new User({ name, email, password });
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const user = new User({ name, email, password: hashedPassword });
     await user.save();
+
+    user.password = undefined; // No enviar la contraseña, incluso encriptada
     res.status(201).json(user);
   } catch (error) {
     console.error('Error al crear el usuario:', error);
@@ -13,29 +20,30 @@ exports.createUser = async (req, res) => {
   }
 };
 
-// Controlador para autenticar un usuario
 exports.authenticateUser = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-    if (!user || user.password !== password) {
+    if (!user || !(await bcrypt.compare(password, user.password))) {
       return res.status(401).send('Credenciales inválidas');
     }
-    // Aquí puedes generar un token de autenticación si es necesario
-    res.status(200).json(user);
+
+    const token = jwt.sign({ userId: user._id }, jwtSecret, { expiresIn: '1h' });
+    res.status(200).json({ user, token });
   } catch (error) {
     console.error('Error al autenticar al usuario:', error);
     res.status(500).send('Error interno del servidor');
   }
 };
 
-// Controlador para obtener un usuario por su ID
 exports.getUser = async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
     if (!user) {
       return res.status(404).send('Usuario no encontrado');
     }
+
+    user.password = undefined;
     res.status(200).json(user);
   } catch (error) {
     console.error('Error al obtener el usuario por ID:', error);
@@ -43,14 +51,19 @@ exports.getUser = async (req, res) => {
   }
 };
 
-// Controlador para actualizar un usuario por su ID
 exports.updateUser = async (req, res) => {
   try {
-    const { name, email, password } = req.body;
-    const user = await User.findByIdAndUpdate(req.params.id, { name, email, password }, { new: true });
+    const updateData = req.body;
+    if (updateData.password) {
+      updateData.password = await bcrypt.hash(updateData.password, saltRounds);
+    }
+    const user = await User.findByIdAndUpdate(req.params.id, updateData, { new: true });
+
     if (!user) {
       return res.status(404).send('Usuario no encontrado');
     }
+
+    user.password = undefined;
     res.status(200).json(user);
   } catch (error) {
     console.error('Error al actualizar el usuario:', error);
@@ -58,7 +71,6 @@ exports.updateUser = async (req, res) => {
   }
 };
 
-// Controlador para eliminar un usuario por su ID
 exports.deleteUser = async (req, res) => {
   try {
     const user = await User.findByIdAndRemove(req.params.id);
