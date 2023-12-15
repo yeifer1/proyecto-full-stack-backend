44605b1de530a063d99d3ada6fb354144f7e1aff
yeison <yeisonferneyhoyos@gmail.com>
2023-12-15 12:05:19 -0500

    ...

--- backend/routes/movies.Routes.js
+++ backend/routes/movies.Routes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { getMovies , getAllMovieId, postMovies, putMovies, deleteMovies } = require('../controllers/movies.Controllers')
-const { protect } = require('../middleware/auth.Middleware')
+const { protect } = require('../middleware/authMiddleware')
 router.get('/', protect, getMovies)
 router.get('/:id', protect, getAllMovieId)
 router.post('/', protect, postMovies)
--- backend/routes/users.Routes.js
+++ backend/routes/users.Routes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { registerUser, loginUser, getUser } = require('../controllers/users.Controllers')
-const { protect } = require('../middleware/auth.Middleware')
+const { protect } = require('../middleware/authMiddleware')
 router.post('/', registerUser)
 router.post('/login', loginUser)
 router.get('/me', protect, getUser)
